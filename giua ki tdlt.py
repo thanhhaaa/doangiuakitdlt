f728971d100fead159e2cae4a868a66694520e9e
minhhieunguyenn <119022055+minhhieunguyenn@users.noreply.github.com>
2022-12-08 21:30:14 +0700

    Update giua ki tdlt.py

--- giua ki tdlt.py
+++ giua ki tdlt.py
@@ -12,79 +12,83 @@ print('giây hiện tại: ', current_second)
 print('năm hiện tại: ', current_year)
 print('tháng hiện tại: ', current_month)
 print('ngày hiện tại: ', current_day)
-gbt = int(input("nhập giờ báo thức"))
-if (gbt<0) or (gbt>23):
-    gbt=int(input("vui lòng nhập lại giờ báo thức"))
-else:
-    pbt = int(input("nhập phút báo thức"))
-    if (pbt<0) or (pbt>60):
-        pbt =int(input("vui lòng nhập lại phút báo thức"))
-    else:
-        giaybt= int(input("nhập giây báo thức"))
-        if (giaybt<0) or (giaybt>60):
-            giaybt= int(input("vui lòng nhập lại giây báo thức"))
-nbt =int(input("nhập năm báo thức"))
-while (nbt < int(current_year)) :
-   nbt= int(input("mời nhập lại năm báo thức"))
-else :
-   tbt = int(input("nhập tháng báo thức"))
-   if (tbt<1) or (tbt>12):
-       tbt = int(input("vui lòng nhập lại tháng báo thức"))
-   else:
-       ngbt = int(input("nhập ngày báo thức"))
-       kq = 0
-       if (nbt % 4 == 0):
-           kq = 1
-       else :
-           kq = 2
-       if (tbt == 2):
-           if (kq == 1):
-               if (ngbt < 1) or (ngbt > 29):
-                   ngbt = int(input("vui lòng nhập lại ngày báo thức"))
-               else:
-                   kq = 3
-           else :
-               if (ngbt < 1) or (ngbt > 28):
-                   ngbt = int(input("vui lòng nhập lại ngày báo thức"))
-               else :
-                   kq = 3
-       if (tbt!= 2):
-           if (tbt < 8):
-               if (tbt%2 == 0):
-                   if (ngbt < 1) or (ngbt > 30):
-                       ngbt = int(input("vui lòng nhập lại ngày báo thức"))
-                   else :
-                       kq = 3
-               else:
-                   if (ngbt < 1) or (ngbt > 31):
-                       ngbt = int(input("vui lòng nhập lại ngày báo thức"))
-                   else:
-                       kq = 3
-           else:
-               if (ngbt < 1) or (ngbt > 31):
-                   ngbt = int(input('vui lòng nhập lại ngày báo thức'))
-               else:
-                   kq = 3
 
-from datetime import datetime
+flag = False
+while flag ==False :
+      gbt = int(input("nhập giờ báo thức"))
+      if (gbt<0) or (gbt>23):
+          gbt=int(input("vui lòng nhập lại giờ báo thức"))
+      else:
+          pbt = int(input("nhập phút báo thức"))
+          if (pbt<0) or (pbt>59):
+              pbt =int(input("vui lòng nhập lại phút báo thức"))
+          else:
+              giaybt= int(input("nhập giây báo thức"))
+              if (giaybt<0) or (giaybt>59):
+                  giaybt= int(input("vui lòng nhập lại giây báo thức"))
+      nbt =int(input("nhập năm báo thức"))
+      while (nbt < int(current_year)) :
+          nbt= int(input("mời nhập lại năm báo thức"))
+      else :
+          tbt = int(input("nhập tháng báo thức"))
+          if (tbt<1) or (tbt>12):
+              tbt = int(input("vui lòng nhập lại tháng báo thức"))
+          else:
+              ngbt = int(input("nhập ngày báo thức"))
+              kq = 0
+          if (nbt % 4 == 0):
+              kq = 1
+          else :
+              kq = 2
+              if (tbt == 2):
+                  if (kq == 1):
+                      if (ngbt < 1) or (ngbt > 29):
+                          ngbt = int(input("vui lòng nhập lại ngày báo thức"))
+                      else:
+                          kq = 3
+                  else :
+                      if (ngbt < 1) or (ngbt > 28):
+                          ngbt = int(input("vui lòng nhập lại ngày báo thức"))
+                      else :
+                          kq = 3
+              if (tbt!= 2):
+                  if (tbt < 8):
+                      if (tbt%2 == 0):
+                          if (ngbt < 1) or (ngbt > 30):
+                              ngbt = int(input("vui lòng nhập lại ngày báo thức"))
+                          else :
+                              kq = 3
+                      else:
+                          if (ngbt < 1) or (ngbt > 31):
+                              ngbt = int(input("vui lòng nhập lại ngày báo thức"))
+                          else:
+                              kq = 3
+                  else:
+                      if (ngbt < 1) or (ngbt > 31):
+                          ngbt = int(input('vui lòng nhập lại ngày báo thức'))
+                      else:
+                          kq = 3
+      from datetime import datetime
 
-today = datetime.today()
-print("Bây giờ là ", today.hour, " giờ ", today.minute, " phút ", today.second, " giây, ngày ", today.day,
-      " tháng ", today.month, " năm ", today.year)
-t1 = datetime(year=today.year, month=today.month, day=today.day, hour=today.hour, minute=today.minute,
-              second=today.second)
-t2 = datetime(year=nbt, month=tbt, day=ngbt, hour=gbt, minute=pbt, second=giaybt)
-t3 = t2 - t1
-t4 = t1 - t2
-if t3 > t4:
-    number_of_days = t3.days
-    number_of_seconds = t3.seconds
-    sogio = t3.seconds // 3600
-    sophut = (t3.seconds % 3600) // 60
-    sogiay = t3.seconds - (sogio * 3600 + sophut * 60)
-    print("Báo thức sẽ kêu sau: ",number_of_days, "ngày" , sogio, "giờ", sophut, "phút", sogiay, "giây")
-else:
-    print('Thời gian đặt báo thức không hợp lệ')
+      today = datetime.today()
+      print("Bây giờ là ", today.hour, " giờ ", today.minute, " phút ", today.second, " giây, ngày ", today.day,
+            " tháng ", today.month, " năm ", today.year)
+      t1 = datetime(year=today.year, month=today.month, day=today.day, hour=today.hour, minute=today.minute,
+                    second=today.second)
+      t2 = datetime(year=nbt, month=tbt, day=ngbt, hour=gbt, minute=pbt, second=giaybt)
+      t3 = t2 - t1
+      t4 = t1 - t2
+      if t3 > t4:
+          number_of_days = t3.days
+          number_of_seconds = t3.seconds
+          sogio = t3.seconds // 3600
+          sophut = (t3.seconds % 3600) // 60
+          sogiay = t3.seconds - (sogio * 3600 + sophut * 60)
+          print("Báo thức sẽ kêu sau: ", number_of_days, "ngày", sogio, "giờ", sophut, "phút", sogiay, "giây")
+          Flag = True
+      else:
+          print('Thời gian đặt báo thức không hợp lệ')
+          Flag = False
 
 flag = False
 while flag == False :
